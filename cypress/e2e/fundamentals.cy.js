97d53a5ca2fd020b8900ea45c1e6492017c22835
mwangizack <ndirangu.zmwangi@gmail.com>
2025-04-09 14:33:12 +0300

    test: Add a test case for fundamentals title

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Fundamentals test', () => {
+  it('Contains Testing Fundamentals', () => {
+    cy.visit('http://localhost:3000/fundamentals')
+    cy.get('[data-test=fundamentals-header]').should('contain.text', 'Testing Fundamentals' )
   })
 })
