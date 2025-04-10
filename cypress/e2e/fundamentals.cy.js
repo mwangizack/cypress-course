b735a7c386831da4392d2128375a023b2f2ee110
mwangizack <ndirangu.zmwangi@gmail.com>
2025-04-10 10:34:57 +0300

    test: Add a test for the accordion

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,26 @@
-describe('Fundamentals test', () => {
-  it('Contains correct header text', () => {
-    cy.visit('/fundamentals')
-    cy.get('[data-test=fundamentals-header]').should('contain.text', 'Testing Fundamentals' )
-  })
-})
+describe("Fundamentals test", () => {
+  beforeEach(() => {
+    cy.visit("/fundamentals");
+  });
+
+  it("Contains correct header text", () => {
+    cy.get("[data-test=fundamentals-header]").should(
+      "contain.text",
+      "Testing Fundamentals"
+    );
+  });
+
+  it("Hides accordion details by default and displays them when clicked", () => {
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+    cy.get("[data-test=accordion-item-1] div[role=button]").click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
+    cy.get("[data-test=accordion-item-1] div[role=button]").click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+  });
+});
