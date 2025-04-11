aad0b277ea9d10c6666c3c4b09f1d6e4ab5a1184
mwangizack <ndirangu.zmwangi@gmail.com>
2025-04-11 10:16:19 +0300

    chore: fix the post request response handling logic

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -1,11 +1,30 @@
-describe('examples test', () => {
+describe("examples test", () => {
+  beforeEach(() => {
+    cy.visit("/overview");
+  });
 
-    it ('Contains the examples heading', () => {
-        cy.visit('/examples')
-        cy.get('h1').contains(/Examples/i)
-    })
+  it("Contains the examples heading", () => {
+    cy.visit("/examples");
+    cy.get("h1").contains(/Examples/i);
+  });
 
-    it ('Multi-page testing', () => {
-        cy.visit('/')
-    })
-})
+  it("Multi-page testing", () => {
+    cy.get("[data-test=nav-why-cypress]").click();
+    cy.location("pathname").should("equal", "/");
+
+    cy.get("[data-test=nav-fundamentals]").click();
+    cy.location("pathname").should("equal", "/fundamentals");
+
+    cy.get("[data-test=nav-forms]").click();
+    cy.location("pathname").should("equal", "/forms");
+
+    cy.get("[data-test=nav-examples]").click();
+    cy.location("pathname").should("equal", "/examples");
+
+    cy.get("[data-test=nav-component]").click();
+    cy.location("pathname").should("equal", "/component");
+
+    cy.get("[data-test=nav-best-practices]").click();
+    cy.location("pathname").should("equal", "/best-practices");
+  });
+});
